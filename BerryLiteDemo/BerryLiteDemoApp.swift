12c04974107090a7b50789d87cac3dab347f7226
kadu-v <kazuonsunday@yahoo.co.jp>
2023-12-23 18:14:26 +0900

    Initial Commit

---
+++ BerryLiteDemo/BerryLiteDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryLiteDemoApp.swift
+//  BerryLiteDemo
+//
+//  Created by 池守和槻 on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BerryLiteDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerryLiteDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BerryLiteDemo
+//
+//  Created by 池守和槻 on 2023/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
